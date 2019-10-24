cfa6bc5233d8fd96bb2636667ef8cb228103ec3f
Phil Newell <newell.philip.86@gmail.com>
2019-10-23 20:47:46 -0400

    setting up bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { NavbarComponent } from './navbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxNavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
